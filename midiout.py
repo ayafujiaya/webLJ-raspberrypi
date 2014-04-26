8a96ce1c54a031e7596a2b05ea63bbccaf6828bc
ayafujiaya <ayafuji.aya@gmail.com>
2014-04-26 05:57:55 +0000

    socket connection

--- midiout.py
+++ midiout.py
@@ -1,38 +1,75 @@
 import pygame
 import pygame.midi
 from time import sleep
+from socketIO_client import SocketIO, BaseNamespace
+import logging
+import json
 
+#log out
+#logging.basicConfig(level=logging.DEBUG)
+
+########### Task List ###########
+#make function each ligt 
+
+#each value initialize
 instrument = 0
 note = 0
 volume = 127
+port = 2
 
+#pygame & pygame.midi initialize
 pygame.init()
 pygame.midi.init()
+
+#output midi device
 for id in range(pygame.midi.get_count()):
   print pygame.midi.get_device_info(id)
 
-port = 2
+#select midi device
 midiOutput = pygame.midi.Output(port, 1)
 midiOutput.set_instrument(instrument)
 
-#.midi_out.write_short(0xb0, 17, 123)
+def response_1(*args):
+    #print args[0]["value"]["color"]
+    color = args[0]["value"]["color"]
+    strobo = args[0]["value"]["strobo"]
+    rotate = args[0]["value"]["rotate"]
+    midiOutput.write_short(0xb0, 3, color)
+    midiOutput.write_short(0xb0, 4, strobo)
+    midiOutput.write_short(0xb0, 5, rotate)    
 
+class Namespace(BaseNamespace):
+
+    def on_connect(self):
+        print '[Connected]'
+
+#socketIO = SocketIO('http://web-lj.ayafuji.com', 80, Namespace)
+#socketIO.emit('bbb', {'xxx': 'yyy'}, on_bbb_response)
+
+
+with SocketIO('http://web-lj.ayafuji.com', 80) as socketIO:
+    socketIO.on('message', response_1);
+    socketIO.wait()
+
+"""
 while(1):
-  for note in range(4, 7):
-    sleep(0.2)
-    volume = 0
-    midiOutput.write_short(0xb0, note, volume)
-    print str(note) + " : " + str(volume)
-    if note == 5:
-      sleep(1)
-      volume = 95
-    else:
-      sleep(1)
-      volume = 100
-    midiOutput.write_short(0xb0, note, volume)
-    print str(note) + " : " + str(volume)
 
-    #midiOutput.note_off(note,volum)
-    #midiOutput.note_on(note,volume)
-    #del midiOutput
-    #pygame.midi.quit()
+    sleep(0.1)
+    midiOutput.write_short(0xb0, 3, 0)
+    sleep(0.1)
+
+    midiOutput.write_short(0xb0, 0, volume)
+    sleep(0.1)
+
+    midiOutput.write_short(0xb0, 7, 100)
+    sleep(0.1)
+    midiOutput.write_short(0xb0, 7, 0)
+    sleep(0.1)
+
+    midiOutput.write_short(0xb0, 4, volume)
+    sleep(0.1)
+
+    volume = volume + 25
+    if volume == 125:
+      volume = 0
+"""
